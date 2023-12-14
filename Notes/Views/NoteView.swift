c2a07d6a9c0c02e3c365dd4b814b4afc649fa259
VadimBaby <vadimdim22@gmail.com>
2023-12-14 11:15:23 +0700

    Removed ChangingViewModel

--- Notes/Views/NoteView.swift
+++ Notes/Views/NoteView.swift
@@ -6,6 +6,43 @@
 //
 
 import SwiftUI
+import Combine
+
+class ChangingViewModel: ObservableObject {
+    @Published var text: String
+    
+    private let note: NoteModel
+    private let updateNote: (_ text: String, _ id: Int) -> Void
+    
+    private var cancellables = Set<AnyCancellable>()
+    
+    init(
+        note: NoteModel,
+        updateNote: @escaping (_ text: String, _ id: Int) -> Void
+    ) {
+        self._text = Published(initialValue: note.text)
+        self.note = note
+        self.updateNote = updateNote
+        
+        addSubscriber()
+    }
+    
+    deinit {
+        cancellables.forEach{ $0.cancel() }
+        
+        cancellables = []
+    }
+    
+    func addSubscriber() {
+        $text
+            .debounce(for: .seconds(0.5), scheduler: DispatchQueue.main)
+            .sink { text in
+                print(text)
+                self.updateNote(text, self.note.id)
+            }
+            .store(in: &cancellables)
+    }
+}
 
 struct NoteView: View {
     @StateObject private var viewModel: ChangingViewModel
